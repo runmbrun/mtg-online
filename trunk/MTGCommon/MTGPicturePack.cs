9782dd334ff3cd94a5613bbdb7336d2e1ba4a604
matthew.m.brown <matthew.m.brown@ed2372da-ac34-11dd-9ee6-b13cfe5fa2ca>
2008-11-08 13:39:15 +0000

    Forgot a file.

---
+++ trunk/MTGCommon/MTGPicturePack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+
+
+namespace MTG
+{
+    public class MTGPicturePack
+    {
+        /*
+        public ArrayList _pictures = null;
+
+        
+        public ArrayList Pictures
+        {
+            get
+            {
+                return _pictures;
+            }
+            set
+            {
+                _pictures.Add(value);
+            }
+        }
+
+        public MTGPicturePack()
+        {
+            _pictures = new ArrayList();
+        }
+
+        public void Add(Image image)
+        {
+            _pictures.Add(image);
+        }*/
+
+        public Dictionary<Int32, Image> Pictures;
+
+        public MTGPicturePack()
+        {
+            Pictures = new Dictionary<Int32, Image>();
+        }
+
+        public void Add(Int32 id, Image image)
+        {
+            Pictures.Add(id, image);
+        }
+    }
+}
