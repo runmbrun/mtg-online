1f22b8592ad2ef1df1f59e9260d6fdd64e6fb304
matthew.m.brown <matthew.m.brown@ed2372da-ac34-11dd-9ee6-b13cfe5fa2ca>
2009-02-20 18:01:28 +0000

    Big changes all around.

--- MTGClient/MTGClientForm.Designer.cs
+++ MTGClient/MTGClientForm.Designer.cs
@@ -46,10 +46,12 @@
             this.dataGridViewDeck = new System.Windows.Forms.DataGridView();
             this.dataGridViewCollection = new System.Windows.Forms.DataGridView();
             this.tabPageStore = new System.Windows.Forms.TabPage();
+            this.buttonBuy = new System.Windows.Forms.Button();
             this.tabPageLobby = new System.Windows.Forms.TabPage();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.buttonBuy = new System.Windows.Forms.Button();
+            this.pictureBoxCardImage = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPageLogin.SuspendLayout();
             this.tabControl2.SuspendLayout();
@@ -58,6 +60,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDeck)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCollection)).BeginInit();
             this.tabPageStore.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCardImage)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -69,17 +73,16 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(787, 566);
+            this.tabControl1.Size = new System.Drawing.Size(836, 487);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPageLogin
             // 
-            this.tabPageLogin.Controls.Add(this.listBoxResults);
             this.tabPageLogin.Controls.Add(this.tabControl2);
             this.tabPageLogin.Location = new System.Drawing.Point(4, 22);
             this.tabPageLogin.Name = "tabPageLogin";
             this.tabPageLogin.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageLogin.Size = new System.Drawing.Size(779, 540);
+            this.tabPageLogin.Size = new System.Drawing.Size(828, 461);
             this.tabPageLogin.TabIndex = 0;
             this.tabPageLogin.Text = "Login";
             this.tabPageLogin.UseVisualStyleBackColor = true;
@@ -87,9 +90,9 @@
             // listBoxResults
             // 
             this.listBoxResults.FormattingEnabled = true;
-            this.listBoxResults.Location = new System.Drawing.Point(0, 389);
+            this.listBoxResults.Location = new System.Drawing.Point(12, 501);
             this.listBoxResults.Name = "listBoxResults";
-            this.listBoxResults.Size = new System.Drawing.Size(776, 147);
+            this.listBoxResults.Size = new System.Drawing.Size(832, 121);
             this.listBoxResults.TabIndex = 4;
             // 
             // tabControl2
@@ -99,7 +102,7 @@
             this.tabControl2.Location = new System.Drawing.Point(6, 6);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(247, 199);
+            this.tabControl2.Size = new System.Drawing.Size(245, 156);
             this.tabControl2.TabIndex = 3;
             // 
             // tabPageRegisteredUser
@@ -112,7 +115,7 @@
             this.tabPageRegisteredUser.Location = new System.Drawing.Point(4, 22);
             this.tabPageRegisteredUser.Name = "tabPageRegisteredUser";
             this.tabPageRegisteredUser.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageRegisteredUser.Size = new System.Drawing.Size(239, 173);
+            this.tabPageRegisteredUser.Size = new System.Drawing.Size(237, 130);
             this.tabPageRegisteredUser.TabIndex = 0;
             this.tabPageRegisteredUser.Text = "Registered User";
             this.tabPageRegisteredUser.UseVisualStyleBackColor = true;
@@ -165,13 +168,14 @@
             this.tabPageNewUser.Location = new System.Drawing.Point(4, 22);
             this.tabPageNewUser.Name = "tabPageNewUser";
             this.tabPageNewUser.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageNewUser.Size = new System.Drawing.Size(239, 173);
+            this.tabPageNewUser.Size = new System.Drawing.Size(237, 130);
             this.tabPageNewUser.TabIndex = 1;
             this.tabPageNewUser.Text = "New User";
             this.tabPageNewUser.UseVisualStyleBackColor = true;
             // 
             // tabPageCollection
             // 
+            this.tabPageCollection.Controls.Add(this.pictureBoxCardImage);
             this.tabPageCollection.Controls.Add(this.label4);
             this.tabPageCollection.Controls.Add(this.label3);
             this.tabPageCollection.Controls.Add(this.dataGridViewDeck);
@@ -179,7 +183,7 @@
             this.tabPageCollection.Location = new System.Drawing.Point(4, 22);
             this.tabPageCollection.Name = "tabPageCollection";
             this.tabPageCollection.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageCollection.Size = new System.Drawing.Size(779, 540);
+            this.tabPageCollection.Size = new System.Drawing.Size(828, 461);
             this.tabPageCollection.TabIndex = 1;
             this.tabPageCollection.Text = "Collection";
             this.tabPageCollection.UseVisualStyleBackColor = true;
@@ -187,7 +191,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 276);
+            this.label4.Location = new System.Drawing.Point(6, 307);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 13);
             this.label4.TabIndex = 1;
@@ -196,7 +200,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 13);
+            this.label3.Location = new System.Drawing.Point(6, 3);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 1;
@@ -205,18 +209,21 @@
             // dataGridViewDeck
             // 
             this.dataGridViewDeck.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewDeck.Location = new System.Drawing.Point(6, 292);
+            this.dataGridViewDeck.Location = new System.Drawing.Point(6, 323);
             this.dataGridViewDeck.Name = "dataGridViewDeck";
-            this.dataGridViewDeck.Size = new System.Drawing.Size(767, 150);
+            this.dataGridViewDeck.Size = new System.Drawing.Size(816, 132);
             this.dataGridViewDeck.TabIndex = 0;
             // 
-            // dataGridViewcollection
+            // dataGridViewCollection
             // 
+            this.dataGridViewCollection.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.dataGridViewCollection.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewCollection.Location = new System.Drawing.Point(6, 29);
-            this.dataGridViewCollection.Name = "dataGridViewcollection";
-            this.dataGridViewCollection.Size = new System.Drawing.Size(767, 175);
+            this.dataGridViewCollection.Location = new System.Drawing.Point(6, 19);
+            this.dataGridViewCollection.Name = "dataGridViewCollection";
+            this.dataGridViewCollection.Size = new System.Drawing.Size(610, 285);
             this.dataGridViewCollection.TabIndex = 0;
+            this.dataGridViewCollection.SelectionChanged += new System.EventHandler(this.dataGridViewCollection_SelectionChanged);
             // 
             // tabPageStore
             // 
@@ -224,33 +231,10 @@
             this.tabPageStore.Location = new System.Drawing.Point(4, 22);
             this.tabPageStore.Name = "tabPageStore";
             this.tabPageStore.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageStore.Size = new System.Drawing.Size(779, 540);
+            this.tabPageStore.Size = new System.Drawing.Size(828, 461);
             this.tabPageStore.TabIndex = 2;
             this.tabPageStore.Text = "Store";
             this.tabPageStore.UseVisualStyleBackColor = true;
-            // 
-            // tabPageLobby
-            // 
-            this.tabPageLobby.Location = new System.Drawing.Point(4, 22);
-            this.tabPageLobby.Name = "tabPageLobby";
-            this.tabPageLobby.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageLobby.Size = new System.Drawing.Size(779, 540);
-            this.tabPageLobby.TabIndex = 3;
-            this.tabPageLobby.Text = "Lobby";
-            this.tabPageLobby.UseVisualStyleBackColor = true;
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 581);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(811, 22);
-            this.statusStrip1.TabIndex = 1;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // notifyIcon1
-            // 
-            this.notifyIcon1.Text = "notifyIcon1";
-            this.notifyIcon1.Visible = true;
             // 
             // buttonBuy
             // 
@@ -262,11 +246,51 @@
             this.buttonBuy.UseVisualStyleBackColor = true;
             this.buttonBuy.Click += new System.EventHandler(this.buttonBuy_Click);
             // 
+            // tabPageLobby
+            // 
+            this.tabPageLobby.Location = new System.Drawing.Point(4, 22);
+            this.tabPageLobby.Name = "tabPageLobby";
+            this.tabPageLobby.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLobby.Size = new System.Drawing.Size(828, 461);
+            this.tabPageLobby.TabIndex = 3;
+            this.tabPageLobby.Text = "Lobby";
+            this.tabPageLobby.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 628);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(857, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // pictureBoxCardImage
+            // 
+            this.pictureBoxCardImage.Location = new System.Drawing.Point(622, 19);
+            this.pictureBoxCardImage.Name = "pictureBoxCardImage";
+            this.pictureBoxCardImage.Size = new System.Drawing.Size(200, 285);
+            this.pictureBoxCardImage.TabIndex = 2;
+            this.pictureBoxCardImage.TabStop = false;
+            // 
             // MTGClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 603);
+            this.ClientSize = new System.Drawing.Size(857, 650);
+            this.Controls.Add(this.listBoxResults);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
             this.Name = "MTGClientForm";
@@ -281,6 +305,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDeck)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCollection)).EndInit();
             this.tabPageStore.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCardImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -309,6 +336,8 @@
         private System.Windows.Forms.TabPage tabPageNewUser;
         private System.Windows.Forms.ListBox listBoxResults;
         private System.Windows.Forms.Button buttonBuy;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.PictureBox pictureBoxCardImage;
     }
 }
 
