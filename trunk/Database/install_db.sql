64569f1729baeaba815eef8fa63dc2543a84bdf8
matthew.m.brown <matthew.m.brown@ed2372da-ac34-11dd-9ee6-b13cfe5fa2ca>
2009-02-25 16:14:42 +0000

    Updated database install script file.

--- trunk/Database/install_db.sql
+++ trunk/Database/install_db.sql
@@ -1,1 +1,35 @@
 -- This is the database install script
+USE [mtg]
+GO
+/****** Object:  Table [dbo].[players]    Script Date: 02/25/2009 10:12:53 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[players](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nchar](10) NOT NULL,
+	[password] [nchar](10) NOT NULL,
+	[admin] [smallint] NOT NULL CONSTRAINT [DF_players_admin]  DEFAULT ((0)),	
+	[online] [smallint] NOT NULL CONSTRAINT [DF_players_online]  DEFAULT ((0)),
+	[collection] [int] NULL,
+ CONSTRAINT [PK_players] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[collections]    Script Date: 02/25/2009 10:12:51 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[collections](
+	[collection_id] [int] NOT NULL,
+	[card_id] [int] NOT NULL,
+ CONSTRAINT [PK_collections] PRIMARY KEY CLUSTERED 
+(
+	[collection_id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
